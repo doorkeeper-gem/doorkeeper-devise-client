a17a1886f678b818a849f10f8bda81c693a400c0
Piotr Jakubowski <piotrj@gmail.com>
2011-11-28 13:34:47 +0100

    Initialize devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,5 @@ DoorkeeperDeviseClient::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
