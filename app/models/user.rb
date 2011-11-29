a7d771b3f8a23781195ff6c179c2abfb5750d6d6
Piotr Jakubowski <piotrj@gmail.com>
2011-11-29 17:11:31 +0100

    Retrieve user email while authenticating via OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   devise :omniauthable
 
   def self.find_or_create_for_doorkeeper_oauth(oauth_data)
-    User.find_or_create_by_doorkeeper_uid(oauth_data.uid)
+    User.find_or_initialize_by_doorkeeper_uid(oauth_data.uid).tap do |user|
+      user.email = oauth_data.info.email
+    end
   end
 
   def update_doorkeeper_credentials(oauth_data)
